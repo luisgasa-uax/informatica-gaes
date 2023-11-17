37fd1a1fb203288547946a876585c410077415f7
Luis García <luisgasa_uax@gmail.com>
2023-11-17 11:39:51 +0100

    doc: TODO Martes y Viernes incorporamos lista de temas pendientes de abarcar

---
+++ Martes/src/MainMartes.java
@@ -0,0 +1,12 @@
+public class MainMartes {
+    // TODO
+    //  Arrays estáticos
+    //  foreach en arrays estáticos
+    //  Escritura y lectura de ficheros de texto
+    //  Clases, Objetos
+    //  Herencia
+    //  ArrayList
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Viernes/src/MainViernes.java
@@ -0,0 +1,16 @@
+public class MainViernes {
+
+    // TODO
+    //  Arrays estáticos
+    //  foreach en arrays estáticos
+    //  Escritura y lectura de ficheros de texto
+    //  Clases, Objetos
+    //  Herencia
+    //  ArrayList
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+    }
+}
